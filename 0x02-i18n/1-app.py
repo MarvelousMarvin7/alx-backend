e1e7323cb97a4a1ba23b2e9e5811516b2b78a398
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-11-11 22:32:32 +0000

    Flask-babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Render index.html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Render index.html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
